b45ae9258b519808cada485354022181bb5d751c
escopec <escopec@utexas.edu>
2021-04-18 18:19:21 -0500

    more updated code

--- analyzeResults.py
+++ analyzeResults.py
@@ -2,33 +2,62 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
+
+def lookupTaskBlock(vertex, subSampleRate):
+    time_of_frame = vertex*1.5*subSampleRate;
+    if time_of_frame <= 180:
+        return 0
+    else:
+        int_part = int(time_of_frame/192)
+        mod_part = (time_of_frame - 180) % 192
+        if mod_part < 12:
+            return 4
+        elif int_part == 1 or int_part == 4:
+            return 3
+        elif int_part == 2 or int_part == 7:
+            return 2
+        elif int_part == 3 or int_part == 6:
+            return 1
+        elif int_part == 5:
+            return 0
+        else:
+            print("error!")
+            return -1
+
+
+def countDistinct(arr):
+
+    arr =np.squeeze(arr)
+    length = len(arr)
+    res = 1
+
+
+    return res
+
+Time_frame_blocks = {'Resting': 0, 'Math': 1, 'Video': 2, 'Memory': 3, 'Instructions': 4}
+
 tol = .00001;
 
-with open('Sub1_Barcode_and_Simplex.pickle', "rb") as fp:   #Pickling
+subSampleRate = 4;
+
+with open('generatedSimplexes/Sub2_Barcode_and_Simplex.pickle', "rb") as fp:   #Pickling
     BD_simplexes = pickle.load(fp)
 
-nonZero_simplexes = []
 
+taskblock_span = np.zeros((len(BD_simplexes), 3))
 for idx, simplex in enumerate(BD_simplexes):
-    if simplex[0] > 0:
-        nonZero_simplexes.append(simplex)
-
-binary_Matrix = np.zeros((len(nonZero_simplexes), 1))
-for idx, simplex in enumerate(nonZero_simplexes):
+    dim = simplex[0]
     b = simplex[1]
     d = simplex[2]
-    complex = simplex[3]
-    frame_locations = np.zeros((len(complex), 1))
-    for i in range(len(complex)):
-        vertex = complex[i]
-        frame_locations[i] = int(vertex/32)
-    if abs(np.var(frame_locations)) < tol:
-        binary_Matrix[idx] = 1
-    else:
-        print("hi")
+    death_complex = simplex[4]
+    frame_locations = np.zeros((len(death_complex), 1))
+    for i in range(len(death_complex)):
+        vertex = death_complex[i]
+        frame_locations[i] =  lookupTaskBlock(vertex, subSampleRate)
+    num_unique = len(np.unique(frame_locations))
+    taskblock_span[idx, :] = [d, num_unique, dim]
 
-
-plt.plot(binary_Matrix)
+plt.scatter(taskblock_span[:, 0], taskblock_span[:, 1], c = taskblock_span[:, 2])
 plt.show()
 
 
--- generateSimplexes.py
+++ generateSimplexes.py
@@ -6,87 +6,98 @@ import gudhi as gd
 from pylab import *
 import matplotlib.pyplot as plt
 import math
-
-lam = .1
-test = np.load('DistanceMatrix_sub01.npz', allow_pickle = True)
-D = test['arr_0'];
-
-for i in range(1017):
-    for j in range(1017):
-        D[i, j] = D[i, j] + lam*np.abs(i - j)
-
-D = D[::4, ::4]
+import time
 
 
-skeleton_protein = gd.RipsComplex(
-    distance_matrix = D,
-    max_edge_length = 30
-)
+def BarcodeAndSimplex(D, save_path):
 
-Rips_simplex_tree_fMRI = skeleton_protein.create_simplex_tree(max_dimension = 3)
+    skeleton_protein = gd.RipsComplex(
+        distance_matrix=D,
+        max_edge_length=.2
+    )
+    Rips_simplex_tree_fMRI = skeleton_protein.create_simplex_tree(max_dimension=2)
 
-print("Simplex Created!")
+    print("Simplex Created!")
+
+    BarCodes_Rips0 = Rips_simplex_tree_fMRI.persistence()
+
+    print("barcode created")
+    filtration = Rips_simplex_tree_fMRI.get_filtration()
+    filtration_list = []
+    for i in filtration:
+        filtration_list.append(i)
+    print("filtration created")
+
+    all_pairs = Rips_simplex_tree_fMRI.persistence_pairs()
+
+    Desired = []
+
+    print("iterating through barcodes!!")
+
+    for i in BarCodes_Rips0:
+        dim = i[0]
+        [b, d] = i[1]
+        if b == 0 and math.isinf(d):
+            continue
+        born_equals = []
+        death_flag = False
+        for j in filtration_list:
+            if j[1] == b:
+                born_equals.append(j[0])
+        born_death_pairs = []
+        for j in all_pairs:
+            for k in born_equals:
+                j[0].sort()
+                k.sort()
+                if j[0] == k:
+                    born_death_pairs.append((k, j[1]))
+
+        for j in born_death_pairs:
+            j[1].sort()
+            for k in filtration_list:
+                k[0].sort()
+                if k[0] == j[1]:
+                    if k[1] == d:
+                        if death_flag:
+                            print("oh no!")
+                        else:
+                            simplex_component = j[0];
+                            death_component = j[1];
+                            death_flag = True;
+        if 'simplex_component' in globals():
+            Desired.append((dim, b, d, simplex_component, death_component))
+            del simplex_component
+        else:
+            print('Death Component not found')
+
+    print("Process Completed")
+
+    #for i in Desired:
+   #     print(str(i))
+
+    with open(save_path, 'wb') as fp:  # Pickling
+        pickle.dump(Desired, fp)
 
 
-BarCodes_Rips0 = Rips_simplex_tree_fMRI.persistence()
-
-
-for i in BarCodes_Rips0:
-    print(str(i))
-
-print("barcode created")
-filtration = Rips_simplex_tree_fMRI.get_filtration()
-print("filtration created")
-filtration2 = Rips_simplex_tree_fMRI.get_filtration()
-
-all_pairs = Rips_simplex_tree_fMRI.persistence_pairs()
-
-Desired = []
-
-print("iterating through barcodes!!")
-
-for i in BarCodes_Rips0:
-    dim = i[0]
-    [b, d] = i[1]
-    if b == 0:
-        print("hi")
-    born_equals = []
-    death_flag = False
-    for j in filtration:
-        if j[1] == b:
-            born_equals.append(j[0])
-    born_death_pairs = []
-    for j in all_pairs:
-        for k in born_equals:
-            j[0].sort()
-            k.sort()
-            if j[0] == k:
-                born_death_pairs.append((k, j[1]))
-
-    for j in born_death_pairs:
-        j[1].sort()
-        for k in filtration2:
-            k[0].sort()
-            if k[0] == j[1]:
-                if k[1] == d:
-                    if death_flag:
-                        print("oh no!")
-                    else:
-                        death_component = k[0];
-                        death_flag = True;
-    if 'death_component' in globals():
-        Desired.append((dim, b,d, death_component))
-        del death_component
-    else:
-        print('hi')
-
-
-print("Process Completed")
+directory = 'distanceMatrices/sub'
+for i in range(3, 21):
+    if i!= 5 and i!= 11:
+        if i < 10:
+            sub_id = '0' + str(i)
+        else:
+            sub_id = str(i)
+        load_path = directory + sub_id + '.npz'
+        test = np.load(load_path, allow_pickle=True)
+        save_path = 'generatedSimplexes/Sub' + sub_id + '.pickle'
+        D = test['arr_0']
+        D = D[::4, ::4]
+        BarcodeAndSimplex(D, save_path)
 
 
 
-with open('Sub1_Barcode_and_Simplex.pickle', 'wb') as fp:   #Pickling
-    pickle.dump(Desired, fp)
+
+
+
 
 
 
